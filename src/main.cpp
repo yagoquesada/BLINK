d086dfe01e78d06035626b93e95983e8cb1f06a8
yagoquesada <79659924+yagoquesada@users.noreply.github.com>
2021-03-10 12:36:01 +0100

    commit 3

--- src/main.cpp
+++ src/main.cpp
@@ -21,8 +21,10 @@ void setup() {
 
 // the loop routine runs over and over again forever:
 void loop() {
+  Serial.println("on");      // print "on" on screen
   digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(1000);               // wait for a second
+  Serial.println("off");     // print "off" on screen
   digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
   delay(2000);               // wait for a second
 }
